9c0f0add40007ef39cf8fa6d696fd2bf31b38521
Tobyjcode <tobias-jes@hotmail.com>
2024-01-29 13:11:38 +0100

    #Sensor Model

--- serverEnvironment/Models/Sensor.cs
+++ serverEnvironment/Models/Sensor.cs
@@ -6,9 +6,9 @@ namespace serverEnvironment.Models
     {
         [Key]
         public int Id { get; set; }
-        public int SensorId { get; set; }
-        public decimal SensorValue { get; set; }
-        public DateTime Created { get; set; }
+        public int TypeId { get; set; }
         public int GroupId { get; set; }
+        public decimal Value { get; set; }
+        public DateTime Created { get; set; }
     }
 }
